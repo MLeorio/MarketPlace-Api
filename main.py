0dc828c041189c39efd00e4116f65e4c4472bc80
MLeorio <fernandafanou@gmail.com>
2022-08-17 17:22:47 +0000

    Reprise du projet : authentification par token

--- main.py
+++ main.py
@@ -1,15 +1,64 @@
 from fastapi import FastAPI, Depends
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from tortoise.contrib.fastapi import register_tortoise
+from passlib.hash import bcrypt
+from model import User, user_Pydantic, userIn_Pydantic
+import jwt
 
-app = FastAPI()
+app = FastAPI(title="Market-Place Api", version="1.0.2")
+
+JWT_SECRET = "myJwtSecret"
+
+register_tortoise(
+    app,
+    db_url='sqlite://db.sqlite3',
+    modules={'models': ['model']},
+    generate_schemas=True,
+    add_exception_handlers=True
+)
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 
+
+@app.post("/users", response_model=user_Pydantic)
+async def create_user(user: userIn_Pydantic):
+    user_obj = User(
+        fullname=user.fullname,
+        email=user.email,
+        tel=user.tel,
+        password=bcrypt.hash(user.password),
+        )
+    await user_obj.save()
+    return await user_Pydantic.from_tortoise_orm(user_obj)
+
+
+async def authenticate_user(username:str, password:str):
+    user = await User.get(email=username)
+    if not user:
+        return False
+    if not user.verif_password(password):
+        return False
+    return user
+
 @app.post("/token")
-async def token(form_data:OAuth2PasswordRequestForm=Depends()):
-    return {"access_token" : form_data.username + 'token'}
+async def generate_token(form_data:OAuth2PasswordRequestForm=Depends()):
+    user = await authenticate_user(form_data.username, form_data.password)
+    
+    if not user:
+        return {'Erreur': "Coordonnees invalides"}
+    
+    user_obj = await userIn_Pydantic.from_tortoise_orm(user)
+    
+    token = jwt.encode(user_obj.dict(), JWT_SECRET)
+    
+    return {'access_token': token, 'token_type':'bearer'}
 
 @app.get("/")
 async def index(token:str = Depends(oauth2_scheme)):
     return {'the_token' : token,
             'hello' : "world"}
+
+@app.get("/users")
+async def get_users():
+    users = await User.all()
+    return users
--- model.py
+++ model.py
@@ -2,8 +2,32 @@ from msilib.schema import tables
 from tortoise import fields
 from tortoise.models import Model
 from tortoise.contrib.pydantic import pydantic_model_creator
+from passlib.hash import bcrypt
 
-# typeOffre = ["Bien", "Service"]
+class User(Model):
+    id = fields.UUIDField(pk=True)
+    fullname = fields.CharField(max_length=100, unique=True)
+    email = fields.CharField(80, unique=True)
+    tel = fields.CharField(50, unique=True)
+    password = fields.CharField(max_length=255)
+    created_at = fields.DatetimeField(auto_now_add=True)
+    updated_at = fields.DatetimeField(auto_now=True)
+    
+    class Meta:
+        table = "USER"
+        
+    def __str__(self) -> str:
+        return self.fullname
+        
+        
+    @classmethod
+    async def get_user(cls, fullname):
+        return cls.get(fullname=fullname)
+    
+    def verif_password(self, password):
+        return bcrypt.verify(password, self.password)
+
+
 
 
 class Seller(Model):
@@ -24,15 +48,15 @@ class Seller(Model):
         return self.nom + " " + self.prenom + "/" + self.statut
 
 
- Enum typeOffre = [
-     ""
- ]
+
+# class Offre(Model):
+#     id = fields.BigIntField(pk=True)
+#     libelle = fields.CharField(max_length=100)
+#     description = fields.CharField(max_length=255)
+#     prix = fields.FloatField()
+#     quantite = fields.IntField()
+#     sort = fields.CharEnumField(typeOffre)
 
 
-class Offre(Model):
-    id = fields.BigIntField(pk=True)
-    libelle = fields.CharField(max_length=100)
-    description = fields.CharField(max_length=255)
-    prix = fields.FloatField()
-    quantite = fields.IntField()
-    sort = fields.CharEnumField(typeOffre)
+user_Pydantic = pydantic_model_creator(User, name='User')
+userIn_Pydantic = pydantic_model_creator(User, name='UserIn', exclude_readonly=True)
